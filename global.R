e607d3c8c2172a1a8a7fec7be6e8d3490fbbb7cd
Mike Trnka <mike.trnka@gmail.com>
2020-04-22 23:06:18 -0700

    First analysis pane functionality added.  Demo data from translocon project.  FDR plot with reactive thresholding.

--- global.R
+++ global.R
@@ -2,8 +2,11 @@
 library(shiny)
 library(googleComputeEngineR)
 library(tidyverse)
+library(DT)
 
 source("R/gceUtils.R")
+source("transloconDemo/touchStoneWorking.R")
+
 
 consoleFile <- "gceRunOutput.txt"
 demoScriptLocation <- "bash ./runDemo.sh"
@@ -13,3 +16,12 @@ if (file.exists(consoleFile)) {
    system2("touch", consoleFile)
 }
 
+nameAccSwap <- function(dataTable) {
+   proteinName.1 <- dataTable$Protein.1
+   proteinName.2 <- dataTable$Protein.2
+   dataTable$Protein.1 <- dataTable$Acc.1
+   dataTable$Protein.2 <- dataTable$Acc.2
+   dataTable$Acc.1 <- proteinName.1
+   dataTable$Acc.2 <- proteinName.2
+   return(dataTable)
+}
